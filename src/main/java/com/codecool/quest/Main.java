3cc7da2d091285dfe661f726f4410625d146119e
Paebo <petibors2@gmail.com>
2019-11-19 11:37:12 +0100

    Merge pick up items into develop

--- src/main/java/com/codecool/quest/Main.java
+++ src/main/java/com/codecool/quest/Main.java
@@ -46,7 +46,7 @@ public class Main extends Application {
         refresh();
         scene.setOnKeyPressed(this::onKeyPressed);
 
-        primaryStage.setTitle("Codecool Quest");
+        primaryStage.setTitle("Codecool quest");
         primaryStage.show();
     }
 
